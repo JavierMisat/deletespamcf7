2403f3674092531d79de532eebbea9d6acaf5fb1
Javier Misat <rjmisal@misena.edu.co>
2019-05-19 18:53:54 -0500

    QUERYS FOR DELETE SPAM

---
+++ querys_delete_spam.sql
@@ -0,0 +1,9 @@
+
+DELETE FROM wpjk_posts WHERE post_status = 'flamingo-spam';
+
+SELECT * FROM wpjk_posts WHERE post_status = 'flamingo-spam';
+
+
+
+ SELECT * FROM wpjk_posts WHERE post_content LIKE '%teamdurst@comcast.net%';
+a
